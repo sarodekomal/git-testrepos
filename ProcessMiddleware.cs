dfcf2e2a7f29cb46abefb6a0c5d4933cc4b3ce42
sarodekomal <Komal.Sarode@extentia.com>
2017-04-20 15:55:39 +0530

    added one more file for fixes

---
+++ ProcessMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProjectForServer
+{
+    public class ProcessMiddleware
+    {
+        private readonly RequestDelegate _nextMiddleware;
+
+        public ProcessMiddleware(RequestDelegate next)
+        {
+            _nextMiddleware = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.WriteAsync("Completed");
+        }
+    }
+}
